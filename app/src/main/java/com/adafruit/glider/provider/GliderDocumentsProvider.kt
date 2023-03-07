93d1f301f29c2ecc205a05dda5ea23d11dc173e5
Antonio <antonio@openroad.es>
2023-03-07 16:25:12 +0100

    Add DocumentProvider base structure

--- app/src/main/java/com/adafruit/glider/provider/GliderDocumentsProvider.kt
+++ app/src/main/java/com/adafruit/glider/provider/GliderDocumentsProvider.kt
@@ -1,37 +1,24 @@
 package com.adafruit.glider.provider
 
 import android.database.Cursor
+import android.database.MatrixCursor
 import android.os.CancellationSignal
 import android.os.ParcelFileDescriptor
 import android.provider.DocumentsContract
 import android.provider.DocumentsProvider
 import com.adafruit.glider.AppContainer
 import com.adafruit.glider.AppContainerImpl
+import com.adafruit.glider.R
+import com.adafruit.glider.provider.ProviderConfig.DEFAULT_DOCUMENT_PROJECTION
+import com.adafruit.glider.provider.ProviderConfig.DEFAULT_ROOT_PROJECTION
+import com.adafruit.glider.provider.ProviderConfig.ROOT_FOLDER_ID
+import com.adafruit.glider.provider.ProviderConfig.ROOT_ID
 import com.adafruit.glider.utils.LogUtils
 
 class GliderDocumentsProvider : DocumentsProvider() {
     // Data - Private
     private val log by LogUtils()
     private lateinit var container: AppContainer
-
-    private val DEFAULT_ROOT_PROJECTION: Array<String> = arrayOf(
-        DocumentsContract.Root.COLUMN_ROOT_ID,
-        DocumentsContract.Root.COLUMN_MIME_TYPES,
-        DocumentsContract.Root.COLUMN_FLAGS,
-        DocumentsContract.Root.COLUMN_ICON,
-        DocumentsContract.Root.COLUMN_TITLE,
-        DocumentsContract.Root.COLUMN_SUMMARY,
-        DocumentsContract.Root.COLUMN_DOCUMENT_ID,
-        DocumentsContract.Root.COLUMN_AVAILABLE_BYTES
-    )
-    private val DEFAULT_DOCUMENT_PROJECTION: Array<String> = arrayOf(
-        DocumentsContract.Document.COLUMN_DOCUMENT_ID,
-        DocumentsContract.Document.COLUMN_MIME_TYPE,
-        DocumentsContract.Document.COLUMN_DISPLAY_NAME,
-        DocumentsContract.Document.COLUMN_LAST_MODIFIED,
-        DocumentsContract.Document.COLUMN_FLAGS,
-        DocumentsContract.Document.COLUMN_SIZE
-    )
 
     override fun onCreate(): Boolean {
         log.info("onCreate")
@@ -45,12 +32,35 @@ class GliderDocumentsProvider : DocumentsProvider() {
 
     override fun queryRoots(projection: Array<out String>?): Cursor {
         log.info("queryRoots projection: $projection")
-        TODO("Not yet implemented")
-    }
 
-    override fun queryDocument(documentId: String?, projection: Array<out String>?): Cursor {
-        log.info("queryDocument documentId: $documentId, projection: $projection")
-        TODO("Not yet implemented")
+        val cursor = MatrixCursor(projection ?: DEFAULT_ROOT_PROJECTION)
+        cursor.newRow().apply {
+            add(DocumentsContract.Root.COLUMN_ROOT_ID, ROOT_ID)
+
+            // You can provide an optional summary, which helps distinguish roots with the same title. You can also use this field for displaying an user account name.
+            add(DocumentsContract.Root.COLUMN_SUMMARY, "File Glider")
+
+            // FLAG_SUPPORTS_CREATE means at least one directory under the root supports creating documents. FLAG_SUPPORTS_RECENTS means your application's most recently used documents will show up in the "Recents" category. FLAG_SUPPORTS_SEARCH allows users to search all documents the application shares.
+            add(
+                DocumentsContract.Root.COLUMN_FLAGS,
+                0/*DocumentsContract.Root.FLAG_SUPPORTS_CREATE or
+                        DocumentsContract.Root.FLAG_SUPPORTS_RECENTS or
+                        DocumentsContract.Root.FLAG_SUPPORTS_SEARCH*/
+            )
+
+            // COLUMN_TITLE is the root title (e.g. Gallery, Drive).
+            add(DocumentsContract.Root.COLUMN_TITLE, "Glider")
+
+            // This document id cannot change after it's shared.
+            add(DocumentsContract.Root.COLUMN_DOCUMENT_ID, ROOT_FOLDER_ID)
+
+            // The child MIME types are used to filter the roots and only present to the user those roots that contain the desired type somewhere in their file hierarchy.
+            //add(DocumentsContract.Root.COLUMN_MIME_TYPES, getChildMimeTypes(baseDir))
+            //add(DocumentsContract.Root.COLUMN_AVAILABLE_BYTES, baseDir.freeSpace)
+            add(DocumentsContract.Root.COLUMN_ICON, R.drawable.glider_logo)
+        }
+
+        return cursor
     }
 
     override fun queryChildDocuments(
@@ -59,6 +69,20 @@ class GliderDocumentsProvider : DocumentsProvider() {
         sortOrder: String?
     ): Cursor {
         log.info("queryChildDocuments parentDocumentId: $parentDocumentId, projection: $projection, sortOrder: $sortOrder")
+
+        val cursor = MatrixCursor(projection ?: DEFAULT_DOCUMENT_PROJECTION)
+        if (parentDocumentId == ROOT_FOLDER_ID) {
+
+        }
+        else {
+
+        }
+
+        return cursor
+    }
+
+    override fun queryDocument(documentId: String?, projection: Array<out String>?): Cursor {
+        log.info("queryDocument documentId: $documentId, projection: $projection")
         TODO("Not yet implemented")
     }
 
